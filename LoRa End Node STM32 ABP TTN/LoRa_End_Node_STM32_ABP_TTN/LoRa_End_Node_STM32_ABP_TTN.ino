ffb61925ef4f0eee1f5ab3097fc75707d030f640
Veera Subrahmanyam Mediboina <r131842@rguktrkv.ac.in>
2021-11-11 18:54:12 +0530

    Update LoRa_End_Node_STM32_ABP_TTN.ino

--- LoRa End Node STM32 ABP TTN/LoRa_End_Node_STM32_ABP_TTN/LoRa_End_Node_STM32_ABP_TTN.ino
+++ LoRa End Node STM32 ABP TTN/LoRa_End_Node_STM32_ABP_TTN/LoRa_End_Node_STM32_ABP_TTN.ino
@@ -1,33 +1,9 @@
 /*******************************************************************************
- * Copyright (c) 2015 Thomas Telkamp and Matthijs Kooijman
- *
- * Permission is hereby granted, free of charge, to anyone
- * obtaining a copy of this document and accompanying files,
- * to do whatever they want with them without any restriction,
- * including, but not limited to, copying, modification and redistribution.
- * NO WARRANTY OF ANY KIND IS PROVIDED.
- *
- * This example sends a valid LoRaWAN packet with payload "Hello,
- * world!", using frequency and encryption settings matching those of
- * the The Things Network.
- *
- * This uses ABP (Activation-by-personalisation), where a DevAddr and
- * Session keys are preconfigured (unlike OTAA, where a DevEUI and
- * application key is configured, while the DevAddr and session keys are
- * assigned/generated in the over-the-air-activation procedure).
- *
- * Note: LoRaWAN per sub-band duty-cycle limitation is enforced (1% in
- * g1, 0.1% in g2), but not the TTN fair usage policy (which is probably
- * violated by this sketch when left running for longer)!
- *
- * To use this sketch, first register your application and device with
- * the things network, to set or generate a DevAddr, NwkSKey and
- * AppSKey. Each device should have their own unique values for these
- * fields.
- *
- * Do not forget to define the radio type correctly in config.h.
- *
+  Modified from the examples of the Arduino LoRa LMIC library by VeeruSubbuAmi
+  More resources: https://electronicsinnovation.com/
+                  https://www.youtube.com/electronicsinnovation
  *******************************************************************************/
+
 
 #include <lmic.h>
 #include <hal/hal.h>
